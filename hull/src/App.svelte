6281607ebbcf8f07597c85d6db729f94bae9ce51
Nathan Rowe <nathanrowenc@gmail.com>
2022-05-16 20:00:11 -0400

    Added physical keyboard control

--- hull/src/App.svelte
+++ hull/src/App.svelte
@@ -1,9 +1,44 @@
 <script>
-	function typeLetter(event) {
+import { onMount } from "svelte";
+
+
+	export let currentKeyDown;
+	export let keyboardKeys = new Map();
+
+	let timer;
+
+	document.addEventListener('keydown', (event) => {
+		const charCode = event.key.charCodeAt(0);
+		if (charCode < 97 || charCode > 122) return; // Only a-z
+
+		typeLetter(event.key);
+	});
+
+	onMount(() => {
+		const k = document.getElementById('keyboard').getElementsByTagName('button');
+		for (let i = 0; i < k.length; i++) {
+			keyboardKeys.set(k[i].innerText.toLowerCase(), k[i]);
+		}
+	})
+
+	function clickKeyboard(event) {
 		if (event.target.tagName !== 'BUTTON') return;
 
-		console.log(event.target.innerText);
-		event.target.classList.add('active')
+		typeLetter(event.target.innerText);
+	}
+
+	function typeLetter(letter) {
+		letter = letter.toLowerCase();
+		if (currentKeyDown == letter) return;
+
+		console.log(letter);
+		currentKeyDown = letter;
+
+		keyboardKeys.get(letter).classList.add('active');
+		window.setTimeout(() => {keyboardKeys.get(letter).classList.remove('active')}, 150);
+
+		window.clearTimeout(timer);
+		timer = setTimeout(() => {currentKeyDown = null}, 800);
 	}
 </script>
 
@@ -14,14 +49,14 @@
 	</header>
 
 	<article>
-		<div class="panorama">
+		<div id="panorama">
 			:)))
 		</div>
-		<div class="keyboard" on:click="{typeLetter}">
+		<div id="keyboard" on:click="{clickKeyboard}">
 			<div class="keyboard-row">
 				<button type="submit">q</button><button>w</button><button>e</button><button>r</button><button>t</button><button>y</button><button>u</button><button>i</button><button>o</button><button>p</button>
 			</div>
-			<div class="keyboard-row">
+			<div class="keyboard-row activez">
 				<button>a</button><button>s</button><button>d</button><button>f</button><button>g</button><button>h</button><button>j</button><button>k</button><button>l</button>
 			</div>
 			<div class="keyboard-row">
@@ -44,7 +79,7 @@
 		font-weight: 100;
 	}
 
-	.keyboard {
+	#keyboard {
 		margin: 0.4rem auto;
 		padding: 0.4rem;
 		width: fit-content;
@@ -53,7 +88,7 @@
 		background-color: var(--black);
 	}
 
-	.keyboard button {
+	#keyboard button {
 		margin: 1px;
 		width: 3rem;
 		height: 3rem;
@@ -64,9 +99,13 @@
 		text-transform: capitalize;
 	}
 
-	.keyboard button:hover, .keyboard button:focus {
+	#keyboard button:hover, #keyboard button:focus {
 		border: 2px solid var(--black);
 		background-color: var(--secondary-color);
+	}
+
+	:global(#keyboard button.active) {
+		background-color: var(--secondary-color-dark) !important;
 	}
 
 	@media (min-width: 640px) {
